befbee04934396eb6f9fac026082bc7f2f30298b
Ankit Bansal <ankit.a.bansal@oracle.com>
2018-08-29 22:01:31 +0530

    Sample app for nodejs with idcs

---
+++ auth.js
@@ -0,0 +1,15 @@
+var ids = {
+  oracle: {
+    "ClientId": "ffbd43e8c6554baa9ea1598d8d4f804c",
+    "ClientSecret": "e9b780af-5eba-4e1f-a8dd-4d49a75d8a10",
+    "ClientTenant": "idcs-5913e33244434c1b8f9389ddf2be3012",
+    "IDCSHost": "https://%tenant%.identity.oraclecloud.com",
+    "AudienceServiceUrl" : "https://idcs-5913e33244434c1b8f9389ddf2be3012.identity.oraclecloud.com",
+    "TokenIssuer": "https://identity.oraclecloud.com/",
+    "scope": "urn:opc:idm:t.user.me openid",
+    "logoutSufix": "/oauth2/v1/userlogout",
+    "redirectURL": "https://testappnew-harshita.uscom-central-1.c9dev1.oc9qadev.com/callback"
+  }
+};
+
+module.exports = ids;
